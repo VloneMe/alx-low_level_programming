b22a3af3e8d1b0e6b61d8ee339a5080fa732ce5a
VloneMe <francisntima@yahoo.com>
2023-02-16 13:44:26 -0800

    No printf, or puts for output

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - main entry point without printf, puts output dispals
+ * Return: Always 1
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+
+	return (1);
+}
