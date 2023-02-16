d849d0af3db35489d83342ecbed56df61f31f274
VloneMe <francisntima@yahoo.com>
2023-02-16 14:20:03 -0800

    All data types

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,12 @@
 #include <unistd.h>
 
 /**
- * main - main entry point without printf, puts output dispals
- * Return: Always 1
- */
+* main - main entry point without printf, puts output dispals
+* Return: Always 1
+*/
 
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-
-	return (1);
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 	stdout);
+return (1);
 }
