9ab2fcf523c72391693ae1a7a6bee4386c5b85e0
VloneMe <francisntima@yahoo.com>
2023-02-17 11:52:06 -0800

    prnt alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * Return: 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char A, a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		putchar(a);
+	for (A = 'A'; A <= 'Z'; A++)
+		putchar(A)
+	putchar('\n');
+
+	return (0);
+}
