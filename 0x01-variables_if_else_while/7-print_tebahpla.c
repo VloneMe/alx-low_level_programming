95921be6b7be2d6d60c444a7a0c1779403731fc7
VloneMe <francisntima@yahoo.com>
2023-02-17 12:46:17 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - main entry point
+ *
+ * Return: 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char Z;
+
+	for (Z = 'z'; Z >= 'a'; Z--)
+		putchar('Z');
+	putchar('\n');
+
+	return (0);
+}
