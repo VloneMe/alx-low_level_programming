4b43fadcdf441594910fd0a2a32823b3b9712f01
VloneMe <francisntima@yahoo.com>
2023-02-17 12:05:57 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point of the program
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char A;
+
+	for (A = 'a'; A <= 'z'; A++)
+		if (A == q && A == e)
+			continue;
+		putchar(A);
+	putchar('\n');
+
+	return (0);
+}
