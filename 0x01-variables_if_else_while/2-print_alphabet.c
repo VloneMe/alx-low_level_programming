b8d589e02be6790839e0a99727f66f61998ef3f7
VloneMe <francisntima@yahoo.com>
2023-02-17 11:28:24 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - main entry of a program.
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+int main(void)
+{
+	char A;
+
+	for (A = 'a'; A <= 'z'; A++)
+		putchar(A, "\n")
+
+	return (0);
+}
