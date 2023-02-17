8ee24dab2fab069150034134e5dce53bdea88a0d
VloneMe <francisntima@yahoo.com>
2023-02-17 12:22:41 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of a program
+ *
+ * Return: 0 (success)
+ *
+ */
+
+init main(void)
+{
+	char n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	print("\n");
+
+	return (0);
+}
