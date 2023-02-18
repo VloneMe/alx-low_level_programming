eee61a0042348487c0f3b9e417def80c6d1c12e0
VloneMe <francisntima@yahoo.com>
2023-02-17 16:58:35 -0800

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,8 +11,8 @@ int main(void)
 {
 	int a, b;
 
-	for (a = '0'; a < '99'; a++)
-		for (b = a + 1; b <= '99'; b++)
+	for (a = 0; a < 99; a++)
+		for (b = a + 1; b <= 99; b++)
 		{
 			putchar((a / 10) + '0');
 			putchar((a % 10) + '0');
