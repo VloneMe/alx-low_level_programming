4778f9d416c1cf7402302cfc72bc759f578c6722
VloneMe <francisntima@yahoo.com>
2023-02-17 16:25:07 -0800

    print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,21 +9,22 @@
 
 int main(void)
 {
-	int A, B, C;
+	int A, B;
 
 	for (A = '0'; A < '9'; A++)
 		for (B = A + 1; B <= '9'; B++)
-			for (C = B + 1; C <= '9'; C++)
-				if (A != B)
-				{
-					putchar(A);
-					putchar(B);
 
-					if (A == '8' && B == '9')
-						continue;
-					putchar(',');
-					putchar(' ');
-				}
+			if (A != B)
+			{
+				putchar(A);
+				putchar(B);
+
+				if (A == '8' && B == '9')
+					continue;
+
+				putchar(',');
+				putchar(' ');
+			}
 	putchar('\n');
 
 	return (0);
