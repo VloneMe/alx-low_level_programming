758c2879a41078bddeb7263235cfe9f99674891a
VloneMe <francisntima@yahoo.com>
2023-02-21 17:24:23 -0800

    print 0 to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -20,19 +20,19 @@ void print_to_98(int n)
 
 			if (n == 98)
 				continue;
-		       	printf(", ");
+			printf(", ");
 		}
 		printf("\n");
 	}
 	else
 	{
-		for (; n > 98; n--)
+		for (; n >= 98; n--)
 		{
 			printf("%d", n);
 
 			if (n == 98)
-			       	continue;
-		       	printf(", ");
+				continue;
+			printf(", ");
 		}
 		printf("\n");
 	}
