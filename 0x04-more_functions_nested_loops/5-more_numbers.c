5530ce10930b933727c814ba2b8bfa5112e1e86a
VloneMe <francisntima@yahoo.com>
2023-02-23 11:58:44 -0800

    numbers times 10 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints more numbers
+ *
+ * Return: 0
+ *
+ */
+
+void more_numbers(void)
+{
+	int n, m;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (m = 0; m <= 14)
+		{
+			if (i < 9)
+				_putchar(m / 10 + '0');
+			_putchar(i % 10 + '0')
+		}
+		_pitchar('\n');
+	}
+}
