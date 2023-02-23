3e1e6621771fe4dc6cdb09b689e9b485c5b2c7e3
VloneMe <francisntima@yahoo.com>
2023-02-23 12:56:24 -0800

    print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - for printing line
+ *
+ * @n: number input
+ *
+ * Return: 0 Always
+ *
+ */
+
+void print_line(int n)
+{
+	int m;
+
+	if (m > 0)
+	{
+		for (m = 0; m < n; m++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
