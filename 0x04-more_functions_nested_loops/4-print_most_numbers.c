cf90da8c6b7442c4f299dbb221e7976bfb80fed3
VloneMe <francisntima@yahoo.com>
2023-02-23 11:42:38 -0800

    print all digit except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -12,8 +12,7 @@ void print_most_numbers(void)
 	int n;
 
 	for (n = '0'; n <= '9')
-		if (n == '2' && n == '4')
-			continue;
-		_putchar(n);
+		if (n != '2' && n != '4')
+			_putchar(n);
 	_putchar('\n');
 }
