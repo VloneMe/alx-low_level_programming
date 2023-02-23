a16e8a9ada322020c9018806ed5e9a4f82b29448
VloneMe <francisntima@yahoo.com>
2023-02-23 06:57:24 -0800

    test either positive or negative

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Checking the function for postive or negative
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	postive_or_negative(n);
+
+	return (0);
+}
