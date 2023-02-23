67901805426f0df424c00abf8d979caf3d993600
VloneMe <francisntima@yahoo.com>
2023-02-23 12:37:49 -0800

    numbers times 10 to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,12 +11,12 @@ void more_numbers(void)
 {
 	int n, m;
 
-	for (n = 0; n < 10; n++)
+	for (n = 1; n < 10; n++)
 	{
 		for (m = 0; m <= 14; m++)
 		{
-			if (m > 9)
-				_putchar(m / 10 + '0');
+			if (m >= 10)
+				_putchar(1);
 			_putchar(m % 10 + '0');
 		}
 		_pitchar('\n');
