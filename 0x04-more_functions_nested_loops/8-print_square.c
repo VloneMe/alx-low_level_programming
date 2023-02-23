cb5aafc9e9cf0d6960b5554b1d801e229c4b16ea
VloneMe <francisntima@yahoo.com>
2023-02-23 13:47:03 -0800

    prints square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_square - This write a function that prints a square with a new line
+ *
+ * @size: This input integer
+ *
+ * Return: Always 0
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	if (size > 0)
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+	else
+		_putchar('\n');
+}
