902f32b5c5dfa44fc1a968d77deeff8d7115be9f
VloneMe <francisntima@yahoo.com>
2023-02-27 09:29:36 -0800

    print reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - reverse function
+ *
+ * @s: input pointer
+ *
+ * Return: nothing
+ *
+ */
+
+void print_rev(char *s)
+{
+	int n = 0;
+
+	while (s[n] != '\n')
+		n++;
+
+	for (n -= 1; n >= 0; n--)
+		_putchar(s[n]);
+
+	_putchar('\n');
+}
