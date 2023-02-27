620a3ccd0b0ef169f98d43f44f256871755c6978
VloneMe <francisntima@yahoo.com>
2023-02-27 15:36:30 -0800

    return string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,13 +12,11 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int len = 0;
+	char *aux = dest;
 
-	while (*(src + len) != '\0')
-	{
-		*(dest + len) = *(src + len);
-		len++;
-	}
-	*(dest + len) = '\n';
-	return (dest);
+	while (*src)
+		*dest++ = *src++;
+
+	return (aux);
+
 }
