55528b61f2166bf5ade157c10f45a7f6f4c19488
VloneMe <francisntima@yahoo.com>
2023-02-27 09:36:39 -0800

    print reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,10 +13,10 @@ void print_rev(char *s)
 {
 	int n = 0;
 
-	while (s[n] != '\n')
+	while (s[n] != '\0')
 		n++;
 
-	for (n = n - 1; n >= 0; n--)
+	for (n -= 1; n >= 0; n--)
 		_putchar(s[n]);
 
 	_putchar('\n');
