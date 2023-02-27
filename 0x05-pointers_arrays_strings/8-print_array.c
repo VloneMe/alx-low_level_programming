bc04983f9a6e3d639a5d2b57f7ffa5ca7c7885bc
VloneMe <francisntima@yahoo.com>
2023-02-27 11:09:15 -0800

    prints array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_array - Array function
+ *
+ * @a: pointer input
+ * @n: interger input
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+
+		if (i < n - 1)
+			printf(", ");
+	}
+
+	_putchar('\n');
+}
