05e8239970a60cf293a796de3648a57660bb3b17
VloneMe <francisntima@yahoo.com>
2023-03-02 03:41:34 -0800

    rot of 13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * rot13 - function that encodes a string using rot13.
+ *
+ * @s: pointer
+ *
+ * Return: string
+ */
+
+char *rot13(char *s)
+{
+	int n, m;
+
+	char data[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (n = 0; s[n] != '\0'; n++)
+	{
+		for (m = 0; m < 52; m++)
+		{
+			if (s[m] == data[m])
+			{
+				s[n] = datarot[m];
+				break;
+			}
+		}
+	}
+
+	return (s);
+
+}
+
