5787e4ca07ce8cc7d053058455414c7f83ac2d65
VloneMe <francisntima@yahoo.com>
2023-03-01 23:35:56 -0800

    String trouper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - function to change lowercase letters to uppercase
+ *
+ * @s: string input
+ *
+ * Return: Converted string
+ *
+ */
+
+char *string_toupper(char *s)
+{
+	char *c = s;
+
+	while (*s)
+	{
+		if (*s >= 'a' && *s <= 'z')
+			*s -= 45;
+		s++;
+	}
+
+	return (c);
+}
