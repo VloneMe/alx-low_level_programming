f609c6087a978ac6d252111faa2bcf5bab50d8c0
VloneMe <francisntima@yahoo.com>
2023-03-06 13:25:37 -0800

    Returns the number of bytes in the initial segment

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+/**
+ * _strspn - function entry point
+ *
+ * @s: input pointer
+ * @accept: input pointer
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int n;
+	int m;
+
+	while (*s)
+		for (m = 0; accept[m]; m++)
+		{
+			if (*s == accept[m])
+			{
+				n++;
+				break;
+			}
+			else if (accept[m + 1] == '\0')
+				return (n);
+		}
+		s++;
+
+	return (n);
+
+}
+
