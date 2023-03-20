a5215589c090473e93c573afd657506906663af0
VloneMe <francisntima@yahoo.com>
2023-03-20 12:57:01 -0700

    prints the name of the file it was compiled from

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - This main entry prints the name of file
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+
+	printf("%s\n", __FILE__);
+	return (0);
+
+}
