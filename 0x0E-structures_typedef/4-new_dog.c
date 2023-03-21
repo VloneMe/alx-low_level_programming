e58d9a9ff93962a9baefc7b923e7e7c1ed7eb01e
VloneMe <francisntima@yahoo.com>
2023-03-21 11:11:19 -0700

    free dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,48 +10,49 @@
  *
  * Return: a pointer to new created dog structure
  * C for copy on Cname and Cowner
+ *
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 
-	dog_t *new_dog;
-	char *Cname, *Cowner;
-	int lenName = 0, lenOwner = 0, i;
+	int n, lenName, lenOwner;
+	struct dog *new_dog = NULL;
 
-	if (name == NULL || owner == NULL)
-		return (NULL);
-
-	while (name[lenName])
+	lenName = 0;
+	while (name[lenName] != '\0')
 		lenName++;
 
-	while (owner[lenOwner])
+	lenOwner = 0;
+	while (owner[lenO] != '\0')
 		lenOwner++;
 
-	new_dog = malloc(sizeof(dog_t));
+	new_dog = malloc(sizeof(struct dog));
 	if (new_dog == NULL)
+	{
+		free(new_dog);
 		return (NULL);
-
-	Cname = malloc(lenName + 1);
-	if (Cname == NULL)
+	}
+	new_dog->name = malloc(lenName + 1);
+	if (new_dog->name == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog);
 		return (NULL);
+	}
 
-	for (i = 0; name[i]; i++)
-		Cname[i] = name[i];
-	Cname[i] = '\0';
-
-	Cowner = malloc(lenOwner + 1);
-	if (Cowner == NULL)
+	new_dog->owner = malloc(lenO + 1);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog->name);
+		free(new_dog->owner);
+		free(new_dog);
 		return (NULL);
-
-	for (i = 0; owner[i]; i++)
-		Cowner[i] = owner[i];
-	Cowner[i] = '\0';
-
-	new_dog->name = Cname;
+	}
+	for (n = 0; n <= lenName; n++)
+		new_dog->name[n] = name[n];
+	for (n = 0; n <= lenOwner; n++)
+		new_dog->owner[n] = owner[n];
 	new_dog->age = age;
-	new_dog->owner = Cowner;
-
 	return (new_dog);
-
 }
