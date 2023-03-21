6c521f6c333d8f758401aa8a3871c234d64ca23a
VloneMe <francisntima@yahoo.com>
2023-03-20 19:50:47 -0700

    dog,h file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,17 +1,13 @@
-#ifndef _DOG_H_
-#define _DOG_H
+#ifndef DOG_H
+#define DOG_H
 
-/**
- * struct dog - structure for a basic dog
- *
- * @name: Dog's name
- * @age: Dog's age
- * @owner: Dog's owner
- *
- * Description: data structure for dog
- *
+
+/** struct dog - dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member 
+ * 	
  */
-
 struct dog
 {
 	char *name;
@@ -21,14 +17,14 @@ struct dog
 
 /**
  * dog_t - typedef for struct dog
- *
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
-#endif_
+#endif
