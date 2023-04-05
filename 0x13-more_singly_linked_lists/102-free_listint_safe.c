85a0ce59252bf2cd26a41d11413bfc6947ce055f
VloneMe <francisntima@yahoo.com>
2023-04-04 19:32:04 -0700

    freee list int sasfe

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,47 +1,88 @@
-#include <stdio.h>
 #include "lists.h"
-#include <stdlib.h>
+
+size_t free_listint_safe(listint_t **h);
 
 /**
- * free_listint_safe - This function frees a list
- * @h: A pointer listint_t structure
- * Return: The size of the list that was free'd
+ * looped_listint_count - This functin counts the number of unique nodes
+ *
+ * @head: A pointer to the head of the listint_t to check.
+ * Return: If the list is not looped - 0.
  */
-size_t free_listint_safe(listint_t **h)
+size_t looped_listint_count(listint_t *head)
 {
-	size_t counter = 0;
-	listint_t *temp;
+	listint_t *tortoise, *hare;
+	size_t nodes = 1;
 
-	temp = *h;
-	while (temp)
+	if (head == NULL || head->next == NULL)
+		return (0);
+
+	tortoise = head->next;
+	hare = (head->next)->next;
+
+	while (hare)
 	{
-		temp = *h;
-		temp = temp->next;
-		free_list(temp);
-		counter++;
-	}
-	*h = NULL;
+		if (tortoise == hare)
+		{
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
 
-	return (counter);
+			tortoise = tortoise->next;
+			while (tortoise != hare)
+			{
+				nodes++;
+				tortoise = tortoise->next;
+			}
+
+			return (nodes);
+		}
+
+		tortoise = tortoise->next;
+		hare = (hare->next)->next;
+	}
+
+	return (0);
 }
 
 /**
- * free_list - This function frees a listint_t recursively
+ * free_listint_safe - this function rees a listint_t list safely (ie.
  *
- * @head: A pointer to the listint_t structure
- * Return: Nothing
+ * @h: A pointer to the address
+ * Return: The size of the list that was freed.
  */
-void free_list(listint_t *head)
+size_t free_listint_safe(listint_t **h)
 {
-	listint_t *temp;
+	listint_t *tmp;
+	size_t nodes, index;
 
-	if (head)
+	nodes = looped_listint_count(*h);
+
+	if (nodes == 0)
 	{
-		temp = head;
-		temp = temp->next;
-		free(temp);
-		free_list(temp);
+		for (; h != NULL && *h != NULL; nodes++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
 	}
 
-	free(head);
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			tmp = (*h)->next;
+			free(*h);
+			*h = tmp;
+		}
+
+		*h = NULL;
+	}
+
+	h = NULL;
+	return (nodes);
 }
