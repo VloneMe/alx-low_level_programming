6c91590dbb7227b202ed69039fb6b78888c4515b
VloneMe <francisntima@yahoo.com>
2023-05-08 18:20:49 -0700

    elf hrader

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -11,10 +11,10 @@
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	Elf64_Ehdr *header;
-	int o, r;
+	int n, m;
 
-	o = open(argv[1], O_RDONLY);
-	if (o == -1)
+	n = open(argv[1], O_RDONLY);
+	if (n == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 		exit(98);
@@ -22,15 +22,15 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	header = malloc(sizeof(Elf64_Ehdr));
 	if (header == NULL)
 	{
-		close_elf(o);
+		close_elf(n);
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 		exit(98);
 	}
-	r = read(o, header, sizeof(Elf64_Ehdr));
-	if (r == -1)
+	m = read(n, header, sizeof(Elf64_Ehdr));
+	if (m == -1)
 	{
 		free(header);
-		close_elf(o);
+		close_elf(n);
 		dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
 		exit(98);
 	}
@@ -47,7 +47,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	print_entry(header->e_entry, header->e_ident);
 
 	free(header);
-	close_elf(o);
+	close_elf(n);
 	return (0);
 }
 
