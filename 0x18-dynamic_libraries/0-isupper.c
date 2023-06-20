8efb5923230fec59023a4db452268a998d5ec13c
VloneMe <francisntima@yahoo.com>
2023-06-19 18:15:30 -0700

    is upperr

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+
+/**
+ * _isupper - This function Checks if a character is uppercase.
+ * @c: Character to be checked.
+ * Return: 1 if c is uppercase, 0 otherwise.
+ */
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
