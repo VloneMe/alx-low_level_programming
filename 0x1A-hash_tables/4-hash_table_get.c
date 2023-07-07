fb25eb884e40f2df95ec02146683688ce8638d2b
VloneMe <francisntima@yahoo.com>
2023-07-07 09:03:55 -0700

    4. >>> ht[betty]

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - This function retrieve the value
+ * that isassociated with a key in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to get the value of.
+ * Return:  NULL Otherwise - the value associated with the key in ht.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	hash_node_t *current_node = ht->array[index];
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
+	if (index >= ht->size)
+		return (NULL);
+
+	while (current_node != NULL)
+	{
+		if (strcmp(current_node->key, key) == 0)
+			return (current_node->value);
+		current_node = current_node->next;
+	}
+
+	return (NULL);
+}
