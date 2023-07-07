434c89d4096e135b2e2be4a4912383e0ac5f6a94
VloneMe <francisntima@yahoo.com>
2023-07-07 09:35:56 -0700

    Updated

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -9,21 +9,30 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
-	hash_node_t *current_node = ht->array[index];
+	unsigned long int index = 0;
+	char *value = NULL;
+	hash_node_t *current_node;
 
-	if (ht == NULL || key == NULL || *key == '\0')
+	if (!ht || !key)
+		return (NULL);
+	else if (strlen(key) == 0)
 		return (NULL);
 
-	if (index >= ht->size)
+	index = key_index((const unsigned char *)key, ht->size);
+
+	if ((ht->array)[index] == NULL)
 		return (NULL);
 
+	current_node = ht->array[index];
 	while (current_node != NULL)
 	{
 		if (strcmp(current_node->key, key) == 0)
-			return (current_node->value);
+		{
+			value = current_node->value;
+			break;
+		}
 		current_node = current_node->next;
 	}
 
-	return (NULL);
+	return (value);
 }
