d68f137860ec87030ac5c2e1768dea11cb80e801
VloneMe <francisntima@yahoo.com>
2023-10-09 08:43:31 -0700

    Search hearders

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef SEARCH_ALGO
+#define SEARCH_ALGO
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stddef.h>
+
+int linear_search(int *array, size_t size, int value);
+int linear_search(int *array, size_t size, int value);
+
+#endif
