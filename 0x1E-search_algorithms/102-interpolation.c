ab02350f4c316a79fc6d5b4ed8e8347745fced0f
VloneMe <francisntima@yahoo.com>
2023-10-10 00:33:56 -0700

    Interpolation search

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -10,37 +10,39 @@
  * Return: The index where 'value' is located,
  * or -1 if 'value' is not found.
  */
-int interpolation_search(int *array, size_t size, int value) {
-    if (array == NULL) {
-        return -1;
-    }
+int interpolation_search(int *array, size_t size, int value)
+{
+	int low = 0;
+	int high = size - 1;
 
-    int low = 0;
-    int high = size - 1;
+	if (array == NULL)
+		return (-1);
 
-    while (low <= high && value >= array[low] && value <= array[high]) {
-        if (low == high) {
-            if (array[low] == value) {
-                printf("Value checked array[%d] = [%d]\n", low, array[low]);
-                return low;
-            }
-            return -1;
-        }
+	while (low <= high && value >= array[low] && value <= array[high])
+	{
+		size_t pos = low + (((double)(high - low) /
+					(array[high] - array[low])) * (value - array[low]));
 
-        size_t pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+		if (low == high)
+		{
+			if (array[low] == value)
+			{
+				printf("Value checked array[%d] = [%d]\n", low, array[low]);
+				return (low);
+			}
+			return (-1);
+		}
 
-        printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 
-        if (array[pos] == value) {
-            return pos;
-        }
+		if (array[pos] == value)
+			return (pos);
 
-        if (array[pos] < value) {
-            low = pos + 1;
-        } else {
-            high = pos - 1;
-        }
-    }
+		if (array[pos] < value)
+			low = pos + 1;
+		else
+			high = pos - 1;
+	}
 
-    return -1;
+	return (-1);
 }
